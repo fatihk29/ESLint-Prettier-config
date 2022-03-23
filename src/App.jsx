bb8472b9f4ee29dfd9e3169ae8813429feb84dda
fatihk29 <fatihkrc7@gmail.com>
2022-03-23 20:51:31 +0300

    configs

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,29 @@
-import './App.css';
 import React from 'react';
 import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  console.log();
-  const a = 2;
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	console.log();
+
+	const a = 2;
+
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				<img src={logo} className='App-logo' alt='logo' />
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<a
+					className='App-link'
+					href='https://reactjs.org'
+					target='_blank'
+					rel='noopener noreferrer'>
+					Learn React
+				</a>
+			</header>
+		</div>
+	);
 }
 
 export default App;
