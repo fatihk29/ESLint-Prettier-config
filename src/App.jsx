6deeb22d93d084d35397a630e388143a0813ce72
fatihk29 <fatihkrc7@gmail.com>
2022-03-25 00:18:49 +0300

    rules2

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,15 @@ function App() {
 
 	t = ++t;
 
+	const lukeSkywalker = 'Luke Skywalker';
+
+	// bad
+	const obj = {
+  	lukeSkywalker: lukeSkywalker,
+	};
+
+	const ob2 = { ...obj };
+
 	return (
 		<div className='App'>
 			<header className='App-header'>
