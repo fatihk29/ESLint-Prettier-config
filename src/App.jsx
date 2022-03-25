b1fca07f45f1384a7f09c18f84eed3a52426ebeb
fatihk29 <fatihkrc7@gmail.com>
2022-03-25 14:49:02 +0300

    airbnb style guides

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,78 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {longNameA, longNameB, longNameC, longNameD, longNameE} from 'path';
 
 function App() {
 	var z = 1;
-	console.log();
 
-	const a = 2
-	a = 3;
-	
+	const item = new Object()
 
-	let t = 4;
-
-	// eslint-disable-next-line no-plusplus
-	t = ++t;
-
-	const lukeSkywalker = 'Luke Skywalker';
-/* eslint no-undef: off */
-/* eslint-env browser */
-	// bad
 	const obj = {
-  	lukeSkywalker: lukeSkywalker,
+		lukeSkywalker: lukeSkywalker,
 	};
 
-	const ob2 = { ...obj };
+	const bad = {
+		'foo': 3,
+		'bar': 4,
+		'data-blah': 5,
+	};
+
+	const original = { a: 1, b: 2 };
+	const copy = Object.assign({}, original, { c: 3 }); // copy => { a: 1, b: 2, c: 3 }
+
+	// no-array-constructor
+	const items = new Array();
+
+	// 5.2 prefer-destructuring
+	const arr = [1, 2, 3, 4];
+
+	// bad
+	const first = arr[0];
+	const second = arr[1];
+
+	console.log(first, second);
+
+	const name = "Capt. Janeway";
+
+	// bad
+	function sayHi(name) {
+  	return 'How are you, ' + name + '?';
+	}
+
+	// bad
+	if (currentUser) {
+  	function test() {
+    	console.log('Nope.');
+  	}
+	};
+
+	// bad
+	const add = new Function('a', 'b', 'return a + b');
+
+	console.log(add(2, 3));
+
+	const subtract = Function('a', 'b', 'return a - b');
+
+	console.log(subtract(2, 3));
+
+	[1, 2, 3].map(function (x) {
+		const y = x + 1;
+		return x * y;
+	});
+
+	// bad
+	class Foo {
+		bar() { return 1; }
+		bar() { return 2; }
+	};
+
+	superPower = new SuperPower();
+
+	const array = [1, 2, 3];
+	let num = 1;
+	num++;
+	--num;
 
 	return (
 		<div className='App'>
